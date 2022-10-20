e2a6f09c5a72c36a0f9457fee4000d77af5f19c2
djamelabbou <djamelabbou@outlook.com>
2022-10-20 21:56:13 +0100

    state error cause of lower case react component

--- src/ui/withLabel/index.jsx
+++ src/ui/withLabel/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 //where does it say text-field ? RIGHT! NOWHERE, SINGLE RESPONSIBILITY
 export default function withLabel(Component) {
-  return function wrappedComponent({ children, focused = false, ...rest }) {
+  return function WrappedComponent({ children, focused = false, ...rest }) {
     const [clicked, setClicked] = useState(false);
     return (
       <label className="" onClick={() => setClicked(!clicked)}>
